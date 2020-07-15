50a6c647ca68b857a482c8e09b9ffd4736da9cc0
Theodore Njanke Betto <nbt60@hotmail.com>
2020-07-15 14:34:55 -0400

    Update App.java
    VOICE-1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello Theodore World!";
+    private final String message = "Hello Theodore1 World!";
 
     public App() {}
 
